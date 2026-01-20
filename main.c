ca0b0d387e83f697380823c35c9ddaf4c680f830
Scott Wagner <scott.wagner@sapo.pt>
2026-01-20 15:38:11 -0100

    Add blinking LED using cyw43 libraries

--- main.c
+++ main.c
@@ -4,30 +4,50 @@
 #include <pico/stdio.h>
 #include <FreeRTOS.h>
 #include <task.h>
+#include <pico/cyw43_arch.h>
 
 #define MY_TASK_PRIORITY  2
 
-static void my_task(void *data);
+static void my_task1(void *data);
+static void my_task0(void *data);
 
 int main() {
     stdio_init_all();
+    cyw43_arch_init();
 
-    xTaskCreate(my_task, "application_task", configMINIMAL_STACK_SIZE, NULL, MY_TASK_PRIORITY, NULL);
+    xTaskCreate(my_task0, "application_task0", configMINIMAL_STACK_SIZE, NULL, MY_TASK_PRIORITY, NULL);
+    xTaskCreate(my_task1, "application_task1", configMINIMAL_STACK_SIZE, NULL, MY_TASK_PRIORITY, NULL);
 
     vTaskStartScheduler();
     // we should never return from FreeRTOS
     panic_unsupported();
 }
 
-void my_task(void *data) {
+void my_task0(void *data) {
     (void)data; // unused parameter
 
-    printf("user task started\n");
+    printf("user task0 started\n");
 
     for (;;) {
         // Do something interesting here
-        printf("My task is running!\n");
-        vTaskDelay(pdMS_TO_TICKS(1000));
+        printf("My task0 is running!\n");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 0);
+        vTaskDelay(pdMS_TO_TICKS(700));
+    }
+    // Do not let a task procedure return
+    vTaskDelete(NULL);
+}
+
+void my_task1(void *data) {
+    (void)data; // unused parameter
+
+    printf("user task1 started\n");
+
+    for (;;) {
+        // Do something interesting here
+        printf("My task1 is running!\n");
+        cyw43_arch_gpio_put(CYW43_WL_GPIO_LED_PIN, 1);
+        vTaskDelay(pdMS_TO_TICKS(1300));
     }
     // Do not let a task procedure return
     vTaskDelete(NULL);
